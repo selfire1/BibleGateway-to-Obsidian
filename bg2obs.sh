e5cc9ef28dc64e54d4199ce3111a0aa2d20206f7
fullbright <full3right@gmail.com>
2022-01-02 18:08:35 +0100

    French : added book synonyms, date written, aliases

--- bg2obs.sh
+++ bg2obs.sh
@@ -56,16 +56,33 @@ book_counter_max=66 # Setting the max amount to 66, since there are 66 books we 
 
 # Book list
 declare -a bookarray # Declaring the Books of the Bible as a list
-bookarray=(Genesis Exodus Leviticus Numbers Deuteronomy Joshua Judges Ruth "1 Samuel" "2 Samuel" "1 Kings" "2 Kings" "1 Chronicles" "2 Chronicles" Ezra Nehemiah Esther Job Psalms Proverbs Ecclesiastes "Song of Solomon" Isaiah Jeremiah Lamentations Ezekiel Daniel Hosea Joel Amos Obadiah Jonah Micah Nahum Habakkuk Zephaniah Haggai Zechariah Malachi Matthew Mark Luke John Acts
-Romans "1 Corinthians" "2 Corinthians" Galatians Ephesians Philippians Colossians "1 Thessalonians" "2 Thessalonians" "1 Timothy" "2 Timothy" Titus Philemon Hebrews James "1 Peter" "2 Peter" "1 John" "2 John" "3 John" Jude Revelation)
+bookarray=(Genese Exode Levitique Numbres Deuteronome Josue Juges Ruth "1 Samuel" "2 Samuel" "1 Rois" "2 Rois" "1 Chroniques" "2 Chroniques" Ezra Nehemie Esther Job Psaumes Proverbes Ecclesiaste "Chants de Salomon" Esaie Jeremie Lamentations Ezekiel Daniel Osee Joel Amos Abdias Jonas Michee Nahum Habacuc Sophonie Aggee Zacharie Malachie Matthieu Marc Luc Jean Actes
+Romains "1 Corinthiens" "2 Corinthiens" Galates Ephesiens Philippiens Colossiens "1 Thessaloniciens" "2 Thessaloniciens" "1 Timothee" "2 Timothee" Tite Philemon Hebreux Jacques "1 Pierre" "2 Pierre" "1 Jean" "2 Jean" "3 Jean" Jude Apocalypse)
 
 # Book chapter list
-declare -a lengtharray # Declaring amount of chapters in each book
+declare -a bookarray # Declaring amount of chapters in each book
 lengtharray=(50 40 27 36 34 24 21 4 31 24 22 25 29 36 10 13 10 42 150 31 12 8 66 52 5 48 12 14 3 9 1 4 7 3 3 3 2 14 4 28 16 24 21 28 16 16 13 6 6 4 4 5 3 6 4 3 1 13 5 5 3 5 1 1 1 22)
 
 # Abbreviation list
 declare -a abbarray # Delaring the abbreviations for each book. You can adapt if you'd like
-abbarray=(Gen Exod Lev Num Deut Josh Judg Ruth "1 Sam" "2 Sam" "1 Kings" "2 Kings" "1 Chron" "2 Chron" Ezr Neh Esth Job Ps Prov Eccles Song Isa Jer Lam Ezek Dan Hos Joel Am Obad Jonah Micah Nah Hab Zeph Hag Zech Mal Matt Mark Luke John Acts Rom "1 Cor" "2 Cor" Gal Ephes Phil Col "1 Thess" "2 Thess" "1 Tim" "2 Tim" Titus Philem Heb James "1 Pet" "2 Pet" "1 John" "2 John" "3 John" Jude Rev)
+abbarray=(Gen Exod Lev Nom Deu Jos Jug Rut "1 Sam" "2 Sam" "1 Roi" "2 Roi" "1 Chro" "2 Chro" Ezr Neh Est Job Ps Prov Eccles Can Esa Jer Lam Ezek Dan Ose Joel Am Abd Jona Mic Nah Hab Zeph Hag Zech Mal Matt Mar Luc Jean Act Rom "1 Cor" "2 Cor" Gal Ephes Phil Col "1 Thess" "2 Thess" "1 Tim" "2 Tim" Tite Philem Heb Jacq "1 Pi" "2 Pie" "1 Jean" "2 Jean" "3 Jean" Jude Apo)
+
+# Book synonyms list
+declare -a booksynarray # Delaring the abbreviations for each book. You can adapt if you'd like
+booksynarray=(Gen Exo Levi Nomb Deut Josu Juge Ruth "1 Samu" "2 Samu" "1 Rois" "2 Rois" "1 Chron" "2 Chron" Ezra Nehe Esth Job Ps Prov Eccles Canti Esai Jere Lame Ezeki Dani Osee Joel Amo Abdi Jonas Mich Nah Hab Zeph Hag Zech Mal Matth Marc Luc Jean Actes Rom "1 Cor" "2 Cor" Gal Ephes Phili Colo "1 Thess" "2 Thess" "1 Tim" "2 Tim" Tite Philem Heb Jacques "1 Pi" "2 Pie" "1 Jean" "2 Jean" "3 Jean" Jude Apoc)
+
+# Book Abbreviation synonyms list
+declare -a abbsynarray # Delaring the abbreviations for each book. You can adapt if you'd like
+abbsynarray=(Gen Exod Lev Num Deut Josh Judg Ruth "1 Sam" "2 Sam" "1 Kings" "2 Kings" "1 Chron" "2 Chron" Ezr Neh Esth Job Ps Prov Eccles Song Isa Jer Lam Ezek Dan Hos Joel Am Obad Jonah Micah Nah Hab Zeph Hag Zech Mal Matt Mark Luke John Acts Rom "1 Cor" "2 Cor" Gal Ephes Phil Col "1 Thess" "2 Thess" "1 Tim" "2 Tim" Titus Philem Heb James "1 Pet" "2 Pet" "1 John" "2 John" "3 John" Jude Rev)
+
+# Book authors list
+declare -a authorarray # Delaring the abbreviations for each book. You can adapt if you'd like
+authorarray=(Moses Moses Moses Moses Moses "Joshua exept death parts" "Samuel, Mathan, Gad" "Samuel, Nathan, Gad" "Samuel, Nathan, Gad" "Samuel, Nathan, Gad" Jeremiah Jeremiah Ezra Ezra Ezr "Nehemiah, Ezra" "Mordecai (Mardoché)" Job "David, Asaph, Ezra, the sons of Korah, Heman, Ethan, Moses and unnamed authors" "Solomon, Agur and Lemuel" Solomon Solomon Isaiah Jeremiah Jeremiah Ezekiel Daniel Hosea Joel Amos Obadiah Jonah Micah Nahum Habakkuk Zephaniah Haggai Zechariah Malachi Matthew "John Mark" Luke "John, the Apostle" "Luke" Paul Paul Paul Paul Paul Paul Paul Paul Paul Paul Paul Paul Paul "Paul, Luke, Barnabas, Apollos" "James" Peter Peter "John, the Apostle" "John, the Apostle" "John, the Apostle" "Jude, the brother of Jesus and James" "John, the Apostle")
+
+# Book date written list
+declare -a datewritenarray # Delaring the abbreviations for each book. You can adapt if you'd like
+datewritenarray=("1445-1405 BC" "1445-1405 BC" "1445-1405 BC" "1445-1405 BC" "1445-1405 BC" "1405-1385 BC" "1043 BC" "1030-1010 BC" "931-722 BC" "931-722 BC" "561-538 BC" "561-538 BC" "450-430 BC" "450-430 BC" "100 BC-AD 100" "424-400 BC" "450-331 BC" "Considered earliest, but date unknown" "1410-450 BC" "971-686 BC" "940-931 BC" "971-965 BC" "700-681 BC" "586-570 BC" "586 BC" "590-570 BC" "536-530 BC" "750-710 BC" "835-796 BC" "750 BC" "850-840 BC" "775 BC" "735-710 BC" "650 BC" "615-605 BC" "635-625 BC" "520 BC" "480-470 BC" "433-424 BC" "AD 50-60" "AD 50-60" "AD 60-61" "AD 80-90" "AD 62" "AD 56" "AD 55" "AD 55-56" "AD 49-50" "AD 60-62" "AD 60-62" "AD 60-62" "AD 51" "AD 51-52" "AD 62-64" "AD 66-67" "AD 62-64" "AD 60-62" "AD 67-69" "AD 44-49" "AD 64-65" "AD 67-68" "AD 90-95" "AD 90-95" "AD 90-95" "AD 68-70" "AD 94-96")
+
 
 if ${verbose} -eq "true"; then
 	echo "Starting download of ${translation} Bible."
@@ -80,8 +97,12 @@ fi
 	fi
 
     book=${bookarray[$book_counter]}
+    booksyn=${booksynarray[$book_counter]}
     maxchapter=${lengtharray[$book_counter]}
     abbreviation=${abbarray[$book_counter]}
+    abbreviationsyn=${abbsynarray[$book_counter]}
+    author=${authorarray[$book_counter]}
+    datewrite=${datewritenarray[$book_counter]}
 
 	if ${verbose} -eq "true"; then
 		echo -n "${book} "
@@ -157,7 +178,7 @@ filename=${export_prefix}$export_number # Setting the filename
   # Navigation format
   export="${title}\n\n$navigation\n***\n\n$text\n\n***\n$navigation"
   if ${aliases} -eq "true"; then
-    alias="---\nAliases: [${book} ${chapter}]\n---\n" # Add other aliases or 'Tags:' here if desired. Make sure to follow proper YAML format.
+    alias="---\nAliases: [${book} ${chapter}, ${booksyn} ${chapter}, ${abbreviationsyn} ${chapter}]\nAuthor: $author\nDate_written: $datewrite\n---\n" # Add other aliases or 'Tags:' here if desired. Make sure to follow proper YAML format.
     export="${alias}${export}"
   fi
   
@@ -185,7 +206,7 @@ filename=${export_prefix}$export_number # Setting the filename
 done # End of the book exporting loop
 
   # Create an overview file for each book of the Bible:
-  overview_file="links: [[The Bible]]\n# ${book}\n\n[[${abbreviation}-01|Start Reading →]]"
+  overview_file="links: [[The Bible]]\nAliases: [${book}, ${booksyn}, ${abbreviationsyn}]\nAuthor: $author\nDate_written: $datewrite\n# ${book}\n\n[[${abbreviation}-01|Start Reading →]]"
   echo -e $overview_file >> "$book.md"
   #mkdir -p ./Scripture ("${translation}")/"${folder_name}"; mv "$book.md" './Scripture ('"${translation}"')/'"${folder_name}"
   mv "$book.md" './Scripture ('"${translation}"')/'"${folder_name}"
