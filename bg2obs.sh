97f873132dceb2504b765056914bd3dd927f6691
selfire1 <try2banonym@gmail.com>
2022-01-04 15:37:07 +0700

    docs: 🌐 Give descriptions for translators

--- bg2obs.sh
+++ bg2obs.sh
@@ -54,20 +54,25 @@ done
 book_counter=0 # Setting the counter to 0
 book_counter_max=66 # Setting the max amount to 66, since there are 66 books we want to import
 
-# "The Bible" in respective language
-biblename="The Bible"
-
 # Book list
 declare -a bookarray # Declaring the Books of the Bible as a list
+declare -a abbarray # Delaring the abbreviations for each book. You can adapt if you'd like
+declare -a lengtharray # Declaring amount of chapters in each book
+
+# -------------------------------------------
+# TRANSLATION: Lists of Names
+# -------------------------------------------
+# For Translation, translate these three lists. Seperated by space and wrapped in quotes if they include whitespace.
+# Name of "The Bible" in your language
+biblename="The Bible"
+# Full names of the books of the Bible
 bookarray=(Genesis Exodus Leviticus Numbers Deuteronomy Joshua Judges Ruth "1 Samuel" "2 Samuel" "1 Kings" "2 Kings" "1 Chronicles" "2 Chronicles" Ezra Nehemiah Esther Job Psalms Proverbs Ecclesiastes "Song of Solomon" Isaiah Jeremiah Lamentations Ezekiel Daniel Hosea Joel Amos Obadiah Jonah Micah Nahum Habakkuk Zephaniah Haggai Zechariah Malachi Matthew Mark Luke John Acts
 Romans "1 Corinthians" "2 Corinthians" Galatians Ephesians Philippians Colossians "1 Thessalonians" "2 Thessalonians" "1 Timothy" "2 Timothy" Titus Philemon Hebrews James "1 Peter" "2 Peter" "1 John" "2 John" "3 John" Jude Revelation)
-
-# Abbreviation list
-declare -a abbarray # Delaring the abbreviations for each book. You can adapt if you'd like
+# Short names of the books of the Bible
 abbarray=(Gen Exod Lev Num Deut Josh Judg Ruth "1 Sam" "2 Sam" "1 Kings" "2 Kings" "1 Chron" "2 Chron" Ezr Neh Esth Job Ps Prov Eccles Song Isa Jer Lam Ezek Dan Hos Joel Am Obad Jonah Micah Nah Hab Zeph Hag Zech Mal Matt Mark Luke John Acts Rom "1 Cor" "2 Cor" Gal Ephes Phil Col "1 Thess" "2 Thess" "1 Tim" "2 Tim" Titus Philem Heb James "1 Pet" "2 Pet" "1 John" "2 John" "3 John" Jude Rev)
+# -------------------------------------------
 
 # Book chapter list
-declare -a lengtharray # Declaring amount of chapters in each book
 lengtharray=(50 40 27 36 34 24 21 4 31 24 22 25 29 36 10 13 10 42 150 31 12 8 66 52 5 48 12 14 3 9 1 4 7 3 3 3 2 14 4 28 16 24 21 28 16 16 13 6 6 4 4 5 3 6 4 3 1 13 5 5 3 5 1 1 1 22)
 
 # Initialise the "The Bible" file for all of the books
